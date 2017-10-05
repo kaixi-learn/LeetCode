8c3f8468343a4b8627a7446cbba83a61da8165a1
= <=>
2017-10-05 21:56:33 +1100

    Improve #445

--- linkedlist-medium/445.Add-Two-Numbers-II-sol1.py
+++ linkedlist-medium/445.Add-Two-Numbers-II-sol1.py
@@ -1,3 +1,9 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -5,104 +11,45 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        DEBUG_FLAG = True
-
-        '''
-            Dump Value from l1 and l2 to List(Stack) l1_list and l2_list
-        '''
-
         l1_list = []
         l2_list = []
 
-
-       # l1压stack
-        while True:
-            if DEBUG_FLAG == True:
-                print("l1 Val = {}".format(l1.val))
+        # l1 压栈
+        while l1:
             l1_list.append(l1.val)
-
-            if l1.next == None :
-                break
             l1 = l1.next
 
-       # l2压stack
-        while True:
-            if DEBUG_FLAG == True:
-                print("l2 Val = {}".format(l2.val))
+
+        # l2 压栈            
+        while l2:
             l2_list.append(l2.val)
-
-            if l2.next == None :
-                break
             l2 = l2.next
-
-        if DEBUG_FLAG == True:
-            print("l1_list = {}".format(l1_list))
-            print("l2_list = {}".format(l2_list))
-
-        i = 0
-        carry = 0
+            
         l3_list = []
-
-
-        '''
-            Extract from List(Stack) and Sum Them
-        '''
-
-        if DEBUG_FLAG == True:
-                print("l1 length = {}".format(len(l1_list)))
-                print("l2 length = {}".format(len(l2_list)))
-
-
-       # l1, l2出stack
-        while True:
-            if len(l1_list) == 0:
-                val1 = 0
+        carry = 0
+        
+        # l1_list，l2_list 出栈，然后加法，再压栈到l3_list        
+        while l1_list or l2_list or carry:
+            if l1_list:
+                l1_val = l1_list.pop() 
             else:
-                val1 = l1_list.pop()
-
-            if len(l2_list) == 0:
-                val2 = 0
+                l1_val = 0
+            
+            if l2_list:               
+                l2_val = l2_list.pop()
             else:
-                val2 = l2_list.pop()
+                l2_val = 0
+                
+            l3_val = l1_val + l2_val + carry
+            l3_list.append(l3_val % 10)
+            carry = l3_val//10
+       
+        # l3_list出栈，然后再生成l3 linkedlist
+        DummyHead = ListNode(0)
+        l3 = DummyHead
+        while l3_list:
+            l3.next = ListNode(l3_list.pop())
+            l3 = l3.next
+        
+        return DummyHead.next
 
-
-            if DEBUG_FLAG == True:
-                print("val1 = {}".format(val1))
-                print("val2 = {}".format(val2))
-
-
-
-       # l1, l2做加法，再stack
-            val3 = val1 + val2 + carry
-            carry = 0
-
-            carry = val3 / 10
-            val3 = val3 % 10
-
-            l3_list.append(val3)
-
-            if len(l1_list) == 0 and len(l2_list) == 0:
-                break
-
-        if DEBUG_FLAG == True:
-            print("l3_list = {}".format(l3_list))
-
-        '''
-            Extract from New List (Stack) and Create a Linked List to Carry these Values
-        '''
-        head = ListNode(0)
-        curr = head
-        while True:
-            val3 = l3_list.pop()
-            if DEBUG_FLAG == True:
-                print("val3 = {}".format(val3))
-                print("l1_list Length = {}".format(len(l3_list)))
-            curr.val = val3
-
-            if len(l3_list) == 0:
-                break
-
-            curr.next = ListNode(0)
-            curr = curr.next
-
-        return head
